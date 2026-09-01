3dcb372a906f5a92b17de6ed64f9cfbe4372fcdf
LidiaVidal <lidiavidal0725@gmail.com>
2026-09-01 13:23:27 -0300

    Cria funcionalidade de abrir e fechar o btn de anotacoes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
 import { mostrarData } from "./modules/date.js";
+import { configurarInteracoesRefeicoes } from "./modules/meals.js";
 
 mostrarData()
+configurarInteracoesRefeicoes()
--- src/scripts/modules/meals.js
+++ src/scripts/modules/meals.js
@@ -1,12 +1,27 @@
 //Cores e botao de anotação
-const refeicoes = document.querySelectorAll('.section_meals')
 
-refeicoes.forEach(section => {
-    const radios = section.querySelectorAll('.meal-status-option')
+export function configurarInteracoesRefeicoes() {
+    const refeicoes = document.querySelectorAll('.section_meals')
 
-    radios.forEach(radio => {
-        radio.addEventListener('change', (e) => {
-            section.dataset.status = e.target.value
+    refeicoes.forEach(section => {
+        const radios = section.querySelectorAll('.meal-status-option')
+
+        radios.forEach(radio => {
+            radio.addEventListener('change', (e) => {
+                section.dataset.status = e.target.value
+            })
         })
+        const btnNotes = section.querySelector('.btn')
+        const notes = section.querySelector('.notes-meals')
+        btnNotes.addEventListener('click', () => {
+            if (notes.style.display == 'block') {
+                notes.style.display = 'none'
+            } else {
+                notes.style.display = 'block'
+            }
+        })
+
     })
-})
+}
+
+
