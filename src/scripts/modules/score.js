919c5ad979a4763fde1ca02d387c63eab2305fa1
LidiaVidal <lidiavidal0725@gmail.com>
2026-09-02 09:39:07 -0300

    Configura estilos para cada pontuacao

--- src/scripts/modules/score.js
+++ src/scripts/modules/score.js
@@ -25,12 +25,11 @@ function pegaRadio() {
 
         
     })
+
     if (refeicaoRegistrada > 0) {
         let score = Math.round(pontosRefeicao / refeicaoRegistrada)
         mostrarScore(score)
     }
-
-    
     
 }
 
@@ -47,13 +46,19 @@ function mostrarScore(pontos) {
 
     const scoreStatus = document.querySelector('.score-status')
     const scoreTexto = document.querySelector('.text-score')
+    const secaoScore = document.querySelector('.section_hero-score')
+
     if(pontos >= 75 && pontos <= 100) {
+        secaoScore.dataset.status = 'focus'
         scoreStatus.textContent = 'Excelente'
         scoreTexto.innerHTML = 'Dia de ouro! <br> Você manteve o foco principal e provou que tem o controle da sua rotina.'
+
     } else if (pontos >= 50 && pontos < 75) {
+        secaoScore.dataset.status = 'balanced'
         scoreStatus.textContent = 'Equilíbrio'
         scoreTexto.innerHTML = 'Dia equilibrado e sustentável. <br> Adaptações inteligentes mantêm o hábito vivo sem neura.'
     } else {
+        secaoScore.dataset.status = 'free'
         scoreStatus.textContent = 'Recalibrar'
         scoreTexto.innerHTML ='Um dia atípico não apaga o seu progresso! <br> O que importa é o próximo passo. Recalibre sem culpa!'
     }
